3c8c93003137d70897c270abf372760770e8db49
Eliot Baker <eliotbaker@gmail.com>
2018-11-20 01:24:12 -0800

    init elisp files

---
+++ eliot-org-variables.el
@@ -0,0 +1,44 @@
+;; @ebaker org head[1-4] colors
+(custom-set-variables '(spacemacs-theme-custom-colors
+                      '(
+                        (head1 . "#b48ead")
+                        (head2 . "#a7a6d4")
+                        (head3 . "#bfebbf")
+                        (head4 . "#f0dfaf")
+                        )))
+(setq org-level-color-stars-only t)
+
+
+;; @ebaker org-mode setyp
+
+;; org-directory
+(setq org-directory "~/org")
+
+(setq org-agenda-files (list (concat org-directory "/todo.org")
+                             (concat org-directory "/yara.org")
+                             (concat org-directory "/emacs.org")
+                             (concat org-directory "/capture-emacs.org")
+                             (concat org-directory "/capture-drafts.org")
+                             (concat org-directory "/capture-beorg.org")
+                             ))
+
+(setq org-archive-location (concat org-directory "/archives/%s_archive::"))
+
+;; TODO remove or fix
+(setq org-todo-keyword-faces
+      '(("TODO" . (:foreground "#ff39a3" :weight bold))
+        ("NEXT" . "#E35DBF")
+        ("DONE" . (:foreground "#A2FF38" :weight bold))
+        ))
+
+;; TODO idk
+(make-face 'eliot-todo-face)
+(set-face-foreground 'eliot-todo-face "#ff39a3")
+;; (make-face 'eliot-done-face)
+;; (set-face-foreground 'eliot-done-face "A2FF38")
+(font-lock-add-keywords nil
+                        '(("TODO" . 'eliot-todo-face)))
+;; (font-lock-add-keywords 'org-mode
+;;                         '(("DONE" . 'eliot-done-face)))
+
+(provide 'eliot-org-variables)
---
+++ eliot-org.el
@@ -0,0 +1,75 @@
+(with-eval-after-load 'org
+
+  ;; @ebaker - custom todo keywords
+  ;; https://emacs.stackexchange.com/questions/31466/all-todos-how-to-set-different-colors-for-different-categories
+  (setq org-todo-keywords
+        '(
+          (sequence "TODO" "NEXT" "|" "DONE")
+          ))
+
+  ;; @ebaker - TODO does this work or need debugging?
+  ;; https://www.computerhope.com/cgi-bin/htmlcolor.pl?c=FF39A3
+  (setq org-tag-faces
+        '(("TODO" . (:foreground "#ff39a3" :weight bold))
+          ("DONE" . (:foreground "#A2FF38" :weight bold))
+          ))
+
+  ;; @ebaker - import org-archive-subtree-hierarchical
+  ;; https://github.com/ebaker/el/org-archive-subtree-hierarchical.el
+  (require 'org-archive-subtree-hierarchical)
+
+
+  ;; @ebaker - custom bullets
+  (setq org-bullets-bullet-list '("◉" "◇" "○" "▻" "⌑"))
+  ;; (setq org-bullets-bullet-list '("▣" "▢" "￭" "⌑" "▪" "▫"))
+  ;; (setq org-bullets-bullet-list '("▢" "￭" "￮" "●" "▣"))
+  ;; (setq org-bullets-bullet-list '("▢" "⬡" "◇" "￮" "▻"))
+
+  ;; @ebaker - folding symbol
+  (setq org-ellipsis " v ")
+  ;; others - ▼ ▽ ▾ ▿ ﹀ v
+
+  ;; @ebaker - custom cature templates
+  ;; TODO - source?
+  (setq org-capture-templates
+        '(("n" "Notes" entry (file "~/org/capture-emacs.org")
+           "* %^{Description} %^g %?
+ded: %U")
+          ("t" "Todo" entry (file "~/org/capture-emacs.org")
+           "* TODO %^{Description} %^g %?
+:PROPERTIES:
+:CREATED: %U
+:END:"))
+        )
+
+  ;; @ebaker - org-refile
+  ;; @ebaker - TODO source?
+  ;; Targets include this file and any file contributing to the agenda - up to 4 levels deep
+  (setq org-refile-targets (quote ((nil :maxlevel . 2)
+                                   (org-agenda-files :maxlevel . 2))))
+  (setq org-completion-use-ido nil)
+  (setq org-refile-use-outline-path 'file)
+  (setq org-outline-path-complete-in-steps nil)
+  (setq org-refile-allow-creating-parent-nodes 'confirm)
+
+  ;; @ebaker agenda commands
+  ;; - https://stackoverflow.com/questions/17182954/write-and-call-function-from-agenda-org-mode
+  (setq org-agenda-custom-commands
+        '(("r" "[r]efile list for captured tasks/notes" tags "+refile")
+          ;; match those tagged with :inbox:, are not scheduled, are not DONE.
+          ("u" "[u]nscheduled tasks" tags "-refile-SCHEDULED={.+}/!-DONE")
+          ("D" "[D]one tasks list" tags "+TODO=\"DONE\"-crypt")
+        )
+      )
+
+  ;; TODO consider a refile & schedule function
+  (defun eliot/org-agenda-refile ()
+    (interactive)
+    (org-agenda-refile))
+
+;; @ebaker - re-map org-meta-return
+(org-defkey org-mode-map [(meta return)] 'org-meta-return)
+
+)
+
+(provide 'eliot-org)
